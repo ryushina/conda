bbabf17f28454771d7941bfa78e5edf3f0d2b1a1
Technical <rustanlacanilao@gmail.com>
2022-11-07 02:33:45 +0800

    upserting properties

--- upsertProperties.py
+++ upsertProperties.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import pyodbc
 import sqlalchemy as sa
-from sqlalchemy import create_engine, MetaData,Table, Column, Numeric, Integer, VARCHAR
+from sqlalchemy import create_engine, MetaData,Table, Column, Numeric, Integer, VARCHAR, text
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.engine import result
 import urllib
 import openpyxl
+from pandas import *
 
 params = urllib.parse.quote_plus("DRIVER={SQL Server Native Client 11.0};"
                                  "SERVER=DESKTOP-T7S1ENJ\SQL2014;"
@@ -28,7 +29,23 @@ table_df = pd.read_sql_table(
     ]
 )
 
-print(table_df)
+df = pd.read_csv('names_extracted.csv')
+
+
+sql_update = text("""
+    UPDATE Employees
+    SET FirstName = :firstname,
+    MiddleName = :middlename,
+    LastName = :lastname
+    WHERE Id = :id
+""")
+
+sql_insert = text("""
+    INSERT INTO Employees (FirstName, MiddleName, LastName,OfficeId)
+    VALUES (:firstname, :middlename, :lastname, :officeid);
+""")
+
+
 
 
 
